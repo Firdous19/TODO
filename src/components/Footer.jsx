1b4c99bcfdee425cdf1e24a5de438f0ba5d3c342
Firdous <firdousrahaman42@gmail.com>
2024-02-17 21:10:08 +0530

    Add Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import Button from "./Button";
+function Footer({ inputColor, inputTextColor }) {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: inputColor,
+          color: inputTextColor,
+        }}
+        className="flex justify-between align-middle p-3.5 pb-4 rounded-b-md"
+      >
+        <h4 className="sm:text-md custom:text-sm text-gray-500">0 items left</h4>
+        <div className="w-40 flex justify-between align-middle">
+          <Button text="All" />
+          <Button text="Active" />
+          <Button text="Completed" />
+        </div>
+        <div>
+          <Button text="Clear Completed" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
